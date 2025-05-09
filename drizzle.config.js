6464a2069616d7e3d242b8559eb5d2508d5eb93e
Mengyuan Tao <mt867@cornell.edu>
2025-05-09 13:59:22 -0400

    connect to supabase

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,13 +1,10 @@
 require("dotenv/config");
 
-const { defineConfig } = require('drizzle-kit');
+const { defineConfig } = require("drizzle-kit");
 
-const url =
-  process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.LOCAL_DATABASE_URL;
+const url = process.env.LOCAL_DATABASE_URL;
 if (!url)
-  throw new Error(
-    `Connection string to ${process.env.NODE_ENV ? 'Neon' : 'local'} Postgres not found.`
-  );
+  throw new Error("LOCAL_DATABASE_URL not found in environment variables.");
 
 /** @type {import('drizzle-kit').Config} */
 module.exports = defineConfig({
